67bbf40efe67894476ae79f8f676668c9c54138b
Mainada109 <136803415+Mainada109@users.noreply.github.com>
2023-07-16 18:53:31 +0300

    Add files via upload

---
+++ Loan.py
@@ -0,0 +1,6 @@
+Age= int(input('Enter the age: '))
+Annual_income=int(input('Enter Annual income: '))
+if Age>=21 and Annual_income>=21000:
+    print("Congratulations you qualify for a loan")
+else: 
+    print("Unfortunately, we are unable to offer you a loan at this time")
